e166e00148c0fc35eb88f73829f96ec11147d31f
kirinrastogi <rastogikirin@gmail.com>
2015-09-14 13:51:59 -0400

    Wing.java

--- Wing.java
+++ Wing.java
@@ -1,10 +1,42 @@
-public class Main {
-    
-    public static void main(String[] args) {
-        
-        System.out.println("Wing is the best IDE. 11/10");
-        
-    }
-    
-}
+package com.swagmen.wing;
 
+import java.awt.Color;
+import java.awt.Container;
+
+import javax.swing.JFrame;
+
+public class Wing extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	static final int HEIGHT = 900;
+	static final int WIDTH = 900;
+
+	WingPanel panel;
+
+	public Wing(String title) {
+		super(title);
+		// Setting the title of the JFrame
+
+		Container container;
+
+		panel = new WingPanel();
+		panel.setBackground(Color.BLACK);
+		container = getContentPane();
+
+		// container.setLayout(null);
+		setLocationByPlatform(true);
+		container.add(panel);
+		container.validate();
+	}
+
+	public static void main(String[] args) {
+		// Game engine loop
+
+		// Instantiate and add the SimplePanel to the frame
+	}
+
+}
