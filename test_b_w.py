6a317697059cce773869bef85ba979aae1b7cf7d
unknown <benjaminemdon@gmail.com>
2015-10-17 22:13:04 -0400

    Changes made

--- test_b_w.py
+++ test_b_w.py
@@ -1,10 +1,9 @@
 from Cimpl import *
 
+
 def black_and_white(img, threshold):
     """ (Cimpl.Image) -> None
-    
-    Best threshold is between 60-80
-    
+
     Convert the specified image to a black-and-white (two-tone) image.
 
     >>> image = load_image(choose_file())
@@ -28,4 +27,45 @@ def black_and_white(img, threshold):
             set_color(img, x, y, black)
         else:     # brightness is between threshold and 255, inclusive
             set_color(img, x, y, white)
+
+
+def scan(img):
+    black_and_white(img, 70)
+    lst = []
+    
+    for x, y, col in img:
+        red, green, blue = col
+        
+        if red == 255:
+            lst.append(sumplusplus(img, x, y, col))
+    
+    largest = 0
+    for number in lst:
+        if largest <= number:
+            largest = number
             
+    return largest
+            
+
+def sumplusplus(img, x, y, col):
+
+    green = (0, 255, 0)
+    r, g, b = col
+    if r == 255:
+        sump = 0
+        set_color(img, x, y, green)
+        sump += 1
+        sump += sumplusplus(img, x, y+1, col)
+        sump += sumplusplus(img, x+1, y+1, col)
+        sump += sumplusplus(img, x+1, y, col)
+        sump += sumplusplus(img, x+1, y-1, col)
+        sump += sumplusplus(img, x, y-1, col)
+        sump += sumplusplus(img, x-1, y-1, col)
+        sump += sumplusplus(img, x-1, y, col)
+        sump += sumplusplus(img, x-1, y+1, col)
+    else:
+        return 0
+
+image = load_image(choose_file())
+scan(image)
+show(image)
