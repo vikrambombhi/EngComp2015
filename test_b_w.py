02e08bbe368bbb7775dc8cf751a2bdc309a20db3
Jacky Chiu <jacky.chiu888@gmail.com>
2015-10-18 00:22:17 -0400

    push

--- test_b_w.py
+++ test_b_w.py
@@ -12,10 +12,6 @@ def black_and_white(img, threshold):
     >>> show(image)
     """
 
-    # Brightness levels range from 0 to 255.
-    # Change the colour of each pixel to black or white, depending on whether
-    # its brightness is in the lower or upper half of this range.
-
     black = create_color(0, 0, 0)
     white = create_color(255, 255, 255)
     number = 0
@@ -79,6 +75,17 @@ def sumplusplus(img, x, y):
         return sump
     else:
         return 0
+
+accepted_value = 900
+
+def final_check(img):
+
+    complete_peice = False
+
+    image_value = scan(img)
+
+    if image_value >= accepted_value:
+        complete_peice = True
 
 image = load_image(choose_file())
 black_and_white(image, 70)
